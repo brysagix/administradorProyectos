f1ad456c3e5f6a300c77ecd014cb2b40385336ee
Bryan <brysagix@hotmail.com>
2021-12-20 19:10:06 -0500

    Agregando actions node plantilla v2

--- src/graphql/ApolloClient.js
+++ src/graphql/ApolloClient.js
@@ -3,7 +3,7 @@ import ApolloClient from "apollo-boost"
 export const client = new ApolloClient({
 //uri: 'https://rickandmortyapi.com/graphql'
 
-
+//
 uri: 'https://back-mintic-ciclo4.herokuapp.com/' || 'http://localhost:4000/'
 //uri: 'https://back-mintic-ciclo4.herokuapp.com/'
 //uri: 'http://localhost:4000/'
